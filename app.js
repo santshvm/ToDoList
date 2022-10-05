8df070e6ebdf7e708355c9ae9ed413728097814e
sancodess-cs <santoshshivam@icloud.com>
2022-10-06 01:17:07 +0530

    implemented the add tasks functionality

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // require the packages that are required - express and body parser 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+var items = [];
 const app = express();
 app.set('view engine', 'ejs');
 
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
     var today = new Date();
@@ -15,9 +15,14 @@ app.get("/", function(req, res) {
         month: "long"
     };
     var day = today.toLocaleDateString("en-US", options);
-    res.render("list", {today:day});
+    res.render("list", {today:day, newItemListArray:items});
 });
 
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 app.listen(3000, function() {
     console.log("Server running at port 3000");;
 });
