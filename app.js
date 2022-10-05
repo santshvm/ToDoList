a2a0e0fb5d7eef82f3c04c20be515f48e4758b96
sancodess-cs <santoshshivam@icloud.com>
2022-10-06 00:55:26 +0530

    added form for post request

--- app.js
+++ app.js
@@ -5,32 +5,16 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 
-var date = new Date();
-var currentDay = date.getDay();
-var day = "";
+
 
 app.get("/", function(req, res) {
-    if(currentDay === 1) {
-        day = "Monday";
-    }
-    else if(currentDay === 2) {
-        day = "Tuesday";
-    }
-    else if(currentDay === 3) {
-        day = "Wednesday";
-    }
-    else if(currentDay === 4) {
-        day = "Thursday";
-    }
-    else if(currentDay === 5) {
-        day = "Friday";
-    }
-    else if(currentDay === 6) {
-        day = "Saturday";
-    }
-    else {
-        day = "Sunday";
-    }
+    var today = new Date();
+    var options = {
+        weekday: "long", 
+        day: "numeric", 
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
     res.render("list", {today:day});
 });
 
